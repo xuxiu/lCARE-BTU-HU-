3186eef3ada4ca34f187509c841a3f544944f20e
xuxiu <xiu.xu@hu-berlin.de>
2015-11-12 19:19:31 +0100

    add

--- LCARE_2b_Simulation/LCARE_2b_Simulation.m
+++ LCARE_2b_Simulation/LCARE_2b_Simulation.m
@@ -8,7 +8,7 @@
 % ---------------------------------------------------------------------
 % Keywords:     simulation, time-series, process, data, basis, model              
 % ---------------------------------------------------------------------
-% See also:     LCARE_2a_rsn
+% See also:     LCARE_2a_rsn, LCARE_2c_Simulation, 
 % ---------------------------------------------------------------------
 % Author:       Andrija Mihoci, Xiu Xu 20150219
 % ---------------------------------------------------------------------
@@ -18,24 +18,28 @@ function [y_t_005, y_t_001] = LCARE_2b_Simulation(Th_005, Th_001)
 er_005 = load('er_005.dat');
 er_001 = load('er_001.dat'); 
 
-y_sim_005      = zeros(size(er_005, 1) + 1, length(er_005)); y_sim_005(1, :) = 0.00045;
-y_sim_plus_005 = zeros(size(y_sim_005)); y_sim_neg_005 = zeros(size(y_sim_005));
+y_sim_005       = zeros(size(er_005, 1) + 1, length(er_005)); 
+y_sim_005(1, :) = 0.00045;
+y_sim_plus_005  = zeros(size(y_sim_005)); 
+y_sim_neg_005   = zeros(size(y_sim_005));
 for t = 1 : 1 : (size(y_sim_005, 1) - 1)   
-  y_sim_plus_005(t, :) = (y_sim_005(t, :) .* (y_sim_005(t, :) > 0)) .^ 2;
-  y_sim_neg_005(t, :)  = (y_sim_005(t, :) .* (y_sim_005(t, :) < 0)) .^ 2;
-  y_sim_005(t + 1, :)  = Th_005(1) + Th_005(2) * y_sim_005(t, :) + ...
-      Th_005(3) * y_sim_plus_005(t, :) + Th_005(4) * y_sim_neg_005(t, :) + er_005(t, :);
+    y_sim_plus_005(t, :) = (y_sim_005(t, :) .* (y_sim_005(t, :) > 0)) .^ 2;
+    y_sim_neg_005(t, :)  = (y_sim_005(t, :) .* (y_sim_005(t, :) < 0)) .^ 2;
+    y_sim_005(t + 1, :)  = Th_005(1) + Th_005(2) * y_sim_005(t, :) + ...
+        Th_005(3) * y_sim_plus_005(t, :) + Th_005(4) * y_sim_neg_005(t, :) + er_005(t, :);
 end
 y_t_005 = y_sim_005(2 : end, :);
 save('y_t_005', 'y_t_005', '-ascii');
 
-y_sim_001      = zeros(size(er_001, 1) + 1, length(er_001)); y_sim_001(1, :) = 0.00045;
-y_sim_plus_001 = zeros(size(y_sim_001)); y_sim_neg_001 = zeros(size(y_sim_001));
+y_sim_001       = zeros(size(er_001, 1) + 1, length(er_001)); 
+y_sim_001(1, :) = 0.00045;
+y_sim_plus_001  = zeros(size(y_sim_001)); 
+y_sim_neg_001   = zeros(size(y_sim_001));
 for t = 1 : 1 : (size(y_sim_001, 1) - 1)   
-  y_sim_plus_001(t, :) = (y_sim_001(t, :) .* (y_sim_001(t, :) > 0)) .^ 2;
-  y_sim_neg_001(t, :)  = (y_sim_001(t, :) .* (y_sim_001(t, :) < 0)) .^ 2;
-  y_sim_001(t + 1, :)  = Th_001(1) + Th_001(2) * y_sim_001(t, :) + ...
-      Th_001(3) * y_sim_plus_001(t, :) + Th_001(4) * y_sim_neg_001(t, :) + er_001(t, :);
+    y_sim_plus_001(t, :) = (y_sim_001(t, :) .* (y_sim_001(t, :) > 0)) .^ 2;
+    y_sim_neg_001(t, :)  = (y_sim_001(t, :) .* (y_sim_001(t, :) < 0)) .^ 2;
+    y_sim_001(t + 1, :)  = Th_001(1) + Th_001(2) * y_sim_001(t, :) + ...
+        Th_001(3) * y_sim_plus_001(t, :) + Th_001(4) * y_sim_neg_001(t, :) + er_001(t, :);
 end
 y_t_001 = y_sim_001(2 : end, :);
 save('y_t_001', 'y_t_001', '-ascii');
---
+++ LCARE_2c_Simulation/README.md
@@ -0,0 +1,56 @@
+
+![http://quantnet.wiwi.hu-berlin.de/style/banner.png](http://quantnet.wiwi.hu-berlin.de/style/banner.png)
+
+## ![qlogo](http://quantnet.wiwi.hu-berlin.de/graphics/quantlogo.png) **LCARE_2c_Simulation**
+
+
+```yaml
+
+Name of Quantlet: LCARE_2c_Simulation
+
+Published in: LCARE
+
+Description: 'Simulates paths of a CARE model with fixed parameter vector.'
+
+Keywords:
+- simulation
+- time-series
+- process
+- data
+- basis
+- model   
+
+See also: 
+- LCARE_2c_Simulation
+
+Author: Andrija Mihoci, Xiu Xu
+
+Submitted:  Thu, November 12 2015 by Xiu Xu
+```
+
+
+
+
+```R
+
+ 
+ 
+clear all; clc;
+Th1_005                    = [-0.01514, -0.01034, -0.31360, -0.06366, 0.00005]';
+Th1_001                    = [-0.02892, -0.00298, -0.14472, -0.00037, 0.00040]';
+[y_t_005_Th1, y_t_001_Th1] = LCARE_2b_Simulation(Th1_005, Th1_001);
+save('y_t_005_Th1', 'y_t_005_Th1', '-ascii');
+save('y_t_001_Th1', 'y_t_001_Th1', '-ascii');
+ 
+Th2_005                    = [-0.00998, -0.05234, -0.85700, 0.56274, 0.00005]';
+Th2_001                    = [-0.02323, 0.10132, -2.43912, 2.63032, 0.00040]';
+[y_t_005_Th2, y_t_001_Th2] = LCARE_2b_Simulation(Th2_005, Th2_001);
+save('y_t_005_Th2', 'y_t_005_Th2', '-ascii');
+save('y_t_001_Th2', 'y_t_001_Th2', '-ascii');
+ 
+Th3_005                    = [0.00000, 0.12149, 0.00421, 0.17589, 0.00005]';
+Th3_001                    = [0.00000, 0.12637, 0.00008, 0.03325, 0.00040]';
+[y_t_005_Th3, y_t_001_Th3] = LCARE_2b_Simulation(Th3_005, Th3_001);
+save('y_t_005_Th3', 'y_t_005_Th3', '-ascii');
+save('y_t_001_Th3', 'y_t_001_Th3', '-ascii');
+ 
